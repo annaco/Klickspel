e85defd88cbf00a6d96cae2e1f1032fd0720e3ea
Natalia Nakagawa <Natalia Nakagawa>
2016-05-24 09:53:17 +0200

    drag and drop

---
+++ public/draganddrop.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	init();
+	dragAndDrop();
+});
+function init(){
+	$('.apple, .orange').draggable({ revert: "invalid"});
+}
+function dragAndDrop() {
+	$('.kastrull').droppable({
+		accept: '.apple',
+		drop:function(event, ui){
+			if (ui.draggable.is('.apple')) {
+				$('.grattis').show();
+				$('.apple').hide();
+			} else {
+                ui.draggable.draggable('option','revert',true);
+			}
+		}
+	});
+}
