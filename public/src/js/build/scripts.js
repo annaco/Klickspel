955108e2274f40315ec5276740ef889ce3d041ef
Natalia Nakagawa <Natalia Nakagawa>
2016-05-27 10:14:10 +0200

    timer stops when the elements are dropped

--- public/src/js/build/scripts.js
+++ public/src/js/build/scripts.js
@@ -1,21 +1,66 @@
+var timer, minutes, points = 0, currentSecond = 0, currentTime = 0, correctItems = 0;
 $(document).ready(function() {
-	init();
-	dragAndDrop();
+	var formData = $("form.login").serialize();
+	console.log(formData);
+	$("#start").on('click', startGame);
+	$('#stop').on('click', endGame);
 });
-function init(){
-	$('.apple, .orange').draggable({ revert: 'invalid'});
+function startGame() {
+	$(".instruction").addClass("close");
+	//Removes the event listener from the start button
+	$('#start').off('click', startGame); 
+
+	//Removes the start button when the game is started
+	$('#start').css({'display':'none'});
+
+	//Starts the timer function
+	timer = setInterval(countTime, 1000);
+	$('#stop').show();
+	makeDraggable();
+	dragAndDrop();
+}
+function countTime() {
+	currentTime++; 
+	var time = "";
+
+	//If less than one minute has passed show seconds else start counting minutes
+	if(currentTime < 60){
+		if(currentTime < 10){
+			time = "00:0" + currentTime;
+		}else{
+			time = "00." + currentTime;
+		}
+	}else{	
+		minutes = Math.floor(currentTime/60);	
+		var seconds = Number(currentTime-60);
+		if(seconds < 10){
+		time = "0" + minutes + ":0" + seconds;
+		}else{
+			time = "0" + minutes + ":" + seconds;
+		}
+	}
+	$('#timer').html(time);
+}
+function makeDraggable(){
+	$('.draggableItem').draggable({revert: 'invalid'});
 }
 function dragAndDrop() {
 	$('.kastrull').droppable({
-		accept: '.apple',
+		accept: '.apple, .chocolate',
 		drop:function(event, ui){
 			if (ui.draggable.is('.apple')) {
+				$('.apple').hide().addClass('ok');
+			} else if (ui.draggable.is('.chocolate')) {
+				$('.chocolate').hide().addClass('ok');
+			}
+			if ($('.ok').length == 2) {
 				$('.grattis').show();
-				$('.apple').hide();
-			} else {
-                ui.draggable.draggable('option','revert',true);
+				setTimeout(endGame);
 			}
 		}
 	});
+}
+function endGame(){
+	clearInterval(timer);
 }
 
